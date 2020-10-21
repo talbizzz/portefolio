b875d73893f581982b1caec63e2d4d52b0cedc06
Aziz Talbi <a.talbi890@gmail.com>
2020-10-21 11:04:14 +0200

    Welcome, Header and About components done√ need to fix the Appointments component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Welcome from './Welcome/Welcome.js';
+import Header from './Header/Header';
+import About from './About/About';
+import Appointments from './Appointments/Appointments.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super(); 
+    this.state= {
+      userActive : false,
+      aboutPos : ""
+    }
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('mousemove', ()=> {
+      // skip the welcome window
+      this.setState({userActive: true});
+      // get the header's height, we'll be used to adapt the display of the About component (mt >= header's height) 
+      let header = document.querySelector('.header');
+      let rect = header.getBoundingClientRect();
+      this.setState({aboutPos : (parseInt(rect.height).toString() + "px") });
+    })
+  }
+
+
+  render(){  
+    return (
+      <div className="App" >
+        {
+          !this.state.userActive
+            ?<div className="header"><Welcome/></div>
+            :(
+              <div>
+                <Header className="header"/>
+                <div style={{marginTop:`${this.state.aboutPos}`}}>
+                  <div><About className="about"/></div>
+                  <div><Appointments className="appointments"/></div>
+                </div>
+              </div>
+            )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
